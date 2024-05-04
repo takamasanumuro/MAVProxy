faf57e26625096e4edbe7157e8ea5d580d38647a
adrianowerneck <adrianowerneck@id.uff.br>
2024-05-04 08:47:25 -0300

    Changed timestamp to ms

--- MAVProxy/modules/mavproxy_data.py
+++ MAVProxy/modules/mavproxy_data.py
@@ -8,34 +8,86 @@ from pymavlink import mavutil
 import sys, traceback
 import requests
 
-from MAVProxy.modules.lib import mp_settings
+# Global variables for buffering data
+data_buffer = []
+BUFFER_SIZE = 40  # Adjust buffer size as needed
 
+organization = "Innomaker"
+bucket = "Innoboat"
+token = "gK8YfMaAl54lo2sgZLiM3Y5CQStHip-7mBe5vbhh1no86k72B4Hqo8Tj1qAL4Em-zGRUxGwBWLkQd1MR9foZ-g=="
 
+def time_to_send(func):
+    def wrapper(*args, **kwargs):
+        time_now = time.time()
+        func(*args, **kwargs)
+        time_after = time.time()
+        print(f"Time to send to InfluxDB: {time_after - time_now:.2f}s")
+    return wrapper
+
+def make_influx_url(ip, port, organization, bucket):
+    return f"http://{ip}:{port}/api/v2/write?org={organization}&bucket={bucket}&precision=ms"
+
+def make_line_protocol(bucket, tags, data):
+    line_protocol = f"{bucket},"
+    for key, value in tags.items():
+        line_protocol += f"{key}={value},"
+    line_protocol = line_protocol[:-1]  #remove last comma
+    line_protocol += " "
+    for key, value in data.items():
+        line_protocol += f"{key}={value},"
+    line_protocol = line_protocol[:-1]  #remove last comma 
+
+    epoch_seconds = int(time.time() * 1000)
+    line_protocol += f" {epoch_seconds}"
+
+    return line_protocol
+
+@time_to_send
+def send_to_influxDB(line_protocol_buffer):
+    if not line_protocol_buffer:
+        return
+
+    ip = "44.221.0.169"
+    port = "8086"
+    url = make_influx_url(ip, port, organization, bucket)
+
+    payload = "\n".join(line_protocol_buffer)
+    print(payload)
+
+    try:
+        headers = {"Authorization": f"Token {token}"}
+        response = requests.post(url, data=payload, headers=headers)
+        #Print nothing in case of 204 (SUCCESS NO RESPONSE)
+        if response.status_code != 204:
+            print(f"Response: {response.text}")
+    except requests.exceptions.RequestException as exception:
+        print(f"Error: {exception}")
+
+def add_to_buffer(payload):
+
+    tags = {"source": "Pixhawk"}
+    data = make_line_protocol(bucket, tags, payload)
+    data_buffer.append(data)
+    if len(data_buffer) >= BUFFER_SIZE:
+        send_to_influxDB(data_buffer)
+        # Clear the buffer after sending
+        data_buffer.clear()
 
 class CustomModule(mp_module.MPModule):
-    #Let's initiate a listener for GPS messages
     def __init__(self, mpstate):
         super(CustomModule, self).__init__(mpstate, "data", "Message listener")
         self.add_command("debug", self.cmd_debug, "Debug", ["on", "off"])
-
         self.debug = True
-        
-    
+
     def cmd_debug(self, args):
-        if args[0] == "on":
-            self.debug = True
-        else:
-            self.debug = False
-    
-    #override print function
+        self.debug = args[0] == "on"
+
     def print(self, msg):
         if self.debug:
             print(msg)
 
     def mavlink_packet(self, m):
-        'handle a MAVLink packet'''
         if m.get_type() == 'GLOBAL_POSITION_INT':
-
             latitude = float(m.lat) / 10000000
             longitude = float(m.lon) / 10000000
             velocity_north_m_s = float(m.vx / 100)
@@ -52,7 +104,8 @@ class CustomModule(mp_module.MPModule):
                 "heading": heading
             }
 
-            send_to_influxDB(payload)
+            #Make line protocol
+            add_to_buffer(payload)
 
         if m.get_type() == 'ATTITUDE':
             roll = float(m.roll)
@@ -71,69 +124,8 @@ class CustomModule(mp_module.MPModule):
                 "yawspeed": yawspeed
             }
 
-            send_to_influxDB(payload)
-
+            add_to_buffer(payload)
 
 def init(mpstate):
     '''initialise module'''
     return CustomModule(mpstate)
-
-def time_to_send(func):
-    def wrapper(*args, **kwargs):
-        time_now = time.time()
-        func(*args, **kwargs)
-        time_after = time.time()
-        print(f"Time to send to InfluxDB: {time_after - time_now:.2f}s")
-    return wrapper
-
-def make_influx_url(ip, port, organization, bucket):
-    return f"http://{ip}:{port}/api/v2/write?org={organization}&bucket={bucket}&precision=s"
-
-def make_line_protocol(bucket, tags, data):
-    line_protocol = f"{bucket},"
-    for key, value in tags.items():
-        line_protocol += f"{key}={value},"
-    line_protocol = line_protocol[:-1]  #remove last comma
-    line_protocol += " "
-    for key, value in data.items():
-        line_protocol += f"{key}={value},"
-    line_protocol = line_protocol[:-1]  #remove last comma 
-
-    epoch_seconds = int(time.time())
-    line_protocol += f" {epoch_seconds}"
-
-    return line_protocol
-
-
-@time_to_send
-def send_to_influxDB(data):
-    organization = "Innomaker"
-    bucket = "Innoboat"
-    token = "gK8YfMaAl54lo2sgZLiM3Y5CQStHip-7mBe5vbhh1no86k72B4Hqo8Tj1qAL4Em-zGRUxGwBWLkQd1MR9foZ-g=="
-
-    ip = "44.221.0.169"
-    port = "8086"
-
-    url = make_influx_url(ip, port, organization, bucket)
-
-    tags = {
-        "source": "Pixhawk"
-    }
-
-    line_protocol = make_line_protocol(bucket, tags, data)
-
-    try:
-
-        headers = {
-             "Authorization": f"Token {token}"
-        }
-
-        response = requests.post(url, data=line_protocol, headers=headers)
-        #Print nothing in case of 204 ( SUCCESS NO RESPONSE)
-        if response.status_code != 204:
-            print(f"Response: {response.text}")
-    except requests.exceptions.RequestException as exception:
-        print(f"Error: {exception}")
-
-
-    
